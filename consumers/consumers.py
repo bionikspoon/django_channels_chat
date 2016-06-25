fce8063aeb6c53716cb8bc7f94150e9e3adb644a
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-06-25 15:48:54 -0500

    use sessions

--- consumers/consumers.py
+++ consumers/consumers.py
@@ -1,24 +1,23 @@
 # coding=utf-8
 from channels import Group
+from channels.auth import channel_session_user_from_http, channel_session_user
 from channels.sessions import channel_session
 
 
-@channel_session
+@channel_session_user_from_http
 def ws_connect(message):
     room = message.content['path'].strip('/')
-    print('message.content', message.content)
-    print('room', room)
     message.channel_session['room'] = room
-    Group('chat-%s' % room).add(message.reply_channel)
+    Group('chat-%s' % message.user.username[0]).add(message.reply_channel)
 
 
-@channel_session
+@channel_session_user
 def ws_message(message):
-    Group('chat-%s' % message.channel_session['room']).send({
+    Group('chat-%s' % message.user.username[0]).send({
         'text': '[user] %s' % message.content['text'],
     })
 
 
-@channel_session
+@channel_session_user
 def ws_disconnect(message):
-    Group('chat-%s' % message.channel_session['room']).discard(message.reply_channel)
+    Group('chat-%s' % message.user.username[0]).discard(message.reply_channel)
--- consumers/views.py
+++ consumers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
+    # request.session['session_key'] = 'abcd'
     return render(request, 'index.html')
