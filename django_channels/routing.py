5dbcd228caf677402d9e79be97a6dda6c1cb056d
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-06-25 15:21:27 -0500

    chat dialogue

--- django_channels/routing.py
+++ django_channels/routing.py
@@ -2,7 +2,7 @@
 from channels import route
 
 channel_routing = [
-    route('websocket.connect', 'consumers.consumers.ws_add'),
+    route('websocket.connect', 'consumers.consumers.ws_connect'),
     route('websocket.receive', 'consumers.consumers.ws_message'),
     route('websocket.disconnect', 'consumers.consumers.ws_disconnect'),
 ]
--- django_channels/settings.py
+++ django_channels/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-b(&y_9k)oyu34qdmt*hnqx2)0p_61z9f#zmd*_c#6^uqr3s$x'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
